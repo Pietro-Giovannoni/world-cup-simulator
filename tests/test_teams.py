3ec9f69b2c81103ae18f0d0c752f56e2f30a4d2d
Pietro Giovannoni <pietro@Mac.fritz.box>
2026-07-12 19:21:06 +0200

    Added tests for the team class

---
+++ tests/test_teams.py
@@ -0,0 +1,88 @@
+from src.worldcup.team import Team
+import pytest
+
+
+def test_team_creation():
+    """
+    Checks the correct declaration of a Team object.
+    """
+    italy = Team(
+        name='Italy',
+        code='ITA',
+        confederation='UEFA',
+        ranking=15,
+        host=False
+    ) 
+
+    assert italy.name == 'Italy'
+    assert italy.code == 'ITA'
+    assert italy.confederation == 'UEFA'
+    assert italy.ranking == 15
+    assert italy.host is False
+
+
+
+def test_team_creation_wrong_name():
+    '''
+    Checks that a wrong Team object declaration raises an exception.
+    '''
+    with pytest.raises(TypeError):
+        Team(
+            name=True,
+            code='DEN',
+            confederation='UEFA',
+            ranking=21,
+            host=False
+        )
+
+def test_team_creation_wrong_code():
+    '''
+    Checks that a wrong Team object declaration raises an exception.
+    '''
+    with pytest.raises(TypeError):
+        Team(
+            name='Denmark',
+            code=123,
+            confederation='UEFA',
+            ranking=21,
+            host=False
+        )
+
+def test_team_creation_wrong_confederation():
+    '''
+    Checks that a wrong Team object declaration raises an exception.
+    '''
+    with pytest.raises(TypeError):
+        Team(
+            name='Denmark',
+            code='DEN',
+            confederation=123,
+            ranking=21,
+            host=False
+        )
+
+def test_team_creation_wrong_ranking():
+    '''
+    Checks that a wrong Team object declaration raises an exception.
+    '''
+    with pytest.raises(TypeError):
+        Team(
+            name='Denmark',
+            code='DEN',
+            confederation='UEFA',
+            ranking='21',
+            host=False
+        )
+
+def test_team_creation_wrong_host():
+    '''
+    Checks that a wrong Team object declaration raises an exception.
+    '''
+    with pytest.raises(TypeError):
+        Team(
+            name='Denmark',
+            code='DEN',
+            confederation='UEFA',
+            ranking=21,
+            host='ABC'
+        )
